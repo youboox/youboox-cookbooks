d005c86696fff0da8d242364ac8e80e98c70e0af
Julien Duponchelle <julien@duponchelle.info>
2013-02-20 15:38:23 +0100

    Deploy foreman by hands

--- deploy_foreman/recipes/default.rb
+++ deploy_foreman/recipes/default.rb
@@ -1,13 +1,17 @@
+gem_package "foreman"
+
 node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'rails'
     Chef::Log.debug("Skipping deploy::rails-rollback application #{application} as it is not a Rails app")
     next
   end
 
-  foreman_export application do
-    log "/var/log/#{application}.log"
-    user deploy[:deploy_to]
-    procfile "#{deploy[:current_path]}/Procfile"
-    process_types ["job"]
+  service application do
+    action [:enable]
+  end
+
+  execute "setup foreman for #{application}" do
+    command "foreman export inittab /etc/init.d/#{application} --user=deploy -l /var/log/ -f '#{deploy[:current_path]}/Procfile'"
+    notifies :restart, resources(:service => application)
   end
 end
