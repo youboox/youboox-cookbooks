7c58bc538479e2b10376a4a460c150ef451e023c
Julien Duponchelle <julien@duponchelle.info>
2013-02-20 14:44:44 +0100

    teste une autre orga

---
+++ deploy_foreman/recipes/deploy_foreman.rb
@@ -0,0 +1,13 @@
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'rails'
+    Chef::Log.debug("Skipping deploy::rails-rollback application #{application} as it is not a Rails app")
+    next
+  end
+
+  foreman_export application do
+    log "/var/log/#{application.log}"
+    user deploy[:deploy_to]
+    procfile "#{deploy[:current_path]}/Procfile"
+    process_types ["job"]
+  end
+end
