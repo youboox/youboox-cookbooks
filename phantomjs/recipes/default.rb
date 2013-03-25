e9e1747b6e5819b75b7ac81c666b92ec4dd3119e
Julien Duponchelle <julien@duponchelle.info>
2013-03-25 16:06:36 +0100

    Installation plus simple de phantomjs

--- phantomjs/recipes/default.rb
+++ phantomjs/recipes/default.rb
@@ -1,14 +1,9 @@
-remote_file "#{Chef::Config[:file_cache_path]}/phantomjs-1.9.0-linux-i686.tar.bz2" do
-  source "https://phantomjs.googlecode.com/files/phantomjs-1.9.0-linux-i686.tar.bz2"
-  notifies :run, "bash[install_phantomjs]", :immediately
-end
-
 bash "install_phantomjs" do
   user "root"
   cwd "/tmp"
   code <<-EOH
-    tar -zjf phantomjs-1.9.0-linux-i686.tar.bz2
+    wget https://phantomjs.googlecode.com/files/phantomjs-1.9.0-linux-i686.tar.bz2
+    tar -xjf phantomjs-1.9.0-linux-i686.tar.bz2
     mv phantomjs-1.9.0-linux-i686/bin/phantomjs /usr/local/bin/
   EOH
-  action :nothing
 end
