ca57f3dcb32354690a510bdc78fc7533446d9c65
Julien Duponchelle <julien@duponchelle.info>
2013-04-18 11:04:53 +0200

    Ajout d'un support pour monit

---
+++ deploy_monit/recipes/default.rb
@@ -0,0 +1,14 @@
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'rails'
+    Chef::Log.debug("Skipping deploy::rails-rollback application #{application} as it is not a Rails app")
+    next
+  end
+
+  execute "setup monit for #{application}" do
+    command "cp /srv/www/#{application}/current/monit/#{deploy[:rails_env]}.monitrc /etc/monit/conf.d/#{application}.monitrc"
+  end
+
+  execute "restart #{application}" do
+    command "monit reload && monit restart #{application}"
+  end
+end
