55f9328b5a26a005a6081435989420e2d059dc29
Julien Duponchelle <julien@duponchelle.info>
2013-06-19 15:19:02 +0300

    Fix recipe

--- phantomjs/recipes/default.rb
+++ phantomjs/recipes/default.rb
@@ -4,6 +4,6 @@ bash "install_phantomjs" do
   code <<-EOH
     wget https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2
     tar -xjf phantomjs-1.9.1-linux-x86_64.tar.bz2
-    mv phantomjs-1.9.0-linux-i686/bin/phantomjs /usr/local/bin/
+    mv phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/local/bin/
   EOH
 end
