9bad0d52af797e3e4ff532bc9ff1dabf11394020
Julien Duponchelle <julien@duponchelle.info>
2013-06-19 12:39:52 +0300

    Upgrade to phantom 64 bits

--- phantomjs/recipes/default.rb
+++ phantomjs/recipes/default.rb
@@ -2,8 +2,8 @@ bash "install_phantomjs" do
   user "root"
   cwd "/tmp"
   code <<-EOH
-    wget https://phantomjs.googlecode.com/files/phantomjs-1.9.0-linux-i686.tar.bz2
-    tar -xjf phantomjs-1.9.0-linux-i686.tar.bz2
+    wget https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2
+    tar -xjf phantomjs-1.9.1-linux-x86_64.tar.bz2
     mv phantomjs-1.9.0-linux-i686/bin/phantomjs /usr/local/bin/
   EOH
 end
