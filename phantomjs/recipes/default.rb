e8a2bf5bd9967837c6953b053e8c67775fd411c7
Julien Duponchelle <julien@duponchelle.info>
2013-07-02 16:42:17 +0200

    Support des architecure multiple pour phantomjs

--- phantomjs/recipes/default.rb
+++ phantomjs/recipes/default.rb
@@ -1,9 +1,11 @@
+arch = kernel['machine'] =~ /x86_64/ ? "x86_64" : "i686"
+
 bash "install_phantomjs" do
   user "root"
   cwd "/tmp"
   code <<-EOH
-    wget https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2
-    tar -xjf phantomjs-1.9.1-linux-x86_64.tar.bz2
-    mv phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/local/bin/
+    wget https://phantomjs.googlecode.com/files/phantomjs-1.9.0-linux-#{arch}.tar.bz2
+    tar -xjf phantomjs-1.9.0-linux-#{arch}.tar.bz2
+    mv phantomjs-1.9.0-linux-#{arch}/bin/phantomjs /usr/local/bin/
   EOH
 end
