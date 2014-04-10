35c7f4a1611e5e05ec4c03dcc69416ef727e8e66
Julien Duponchelle <julien@duponchelle.info>
2014-04-10 14:25:48 +0200

    Patch code

--- ephemeral_tmp/metadata.rb
+++ ephemeral_tmp/metadata.rb
@@ -1,6 +1,5 @@
-maintainer        "Donald Piret"
-maintainer_email  "donald@roomorama.com"
-license           "Apache 2.0"
+maintainer        "Youboox"
+maintainer_email  "tech@youboox.fr"
 description       "Set up the tmp directory on the ephemeral drive"
 version           "0.1"
 recipe            "ephemeral_tmp::default", "Set up the ephemeral tmp"
